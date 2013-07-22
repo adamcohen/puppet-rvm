7b38a9a10aeb9cef7753413aae47e1005faf97a1
Adam Cohen <acohen@au.westfield.com>
2013-07-22 16:42:03 +1000

    disable inclusion of mod_ssl package since it interferes with puppetlabs apache module

--- manifests/passenger/apache/centos/pre.pp
+++ manifests/passenger/apache/centos/pre.pp
@@ -2,5 +2,7 @@ class rvm::passenger::apache::centos::pre {
   # Dependencies
   if ! defined(Package['httpd'])       { package { 'httpd':       ensure => present } }
   if ! defined(Package['httpd-devel']) { package { 'httpd-devel': ensure => present } }
-  if ! defined(Package['mod_ssl'])     { package { 'mod_ssl':     ensure => present } }
+  # disable until I figure out how to resolves this dependency issue
+  # with puppetlabs-apache described here http://tinyurl.com/kewgjjo
+  # if ! defined(Package['mod_ssl'])     { package { 'mod_ssl':     ensure => present } }
 }
